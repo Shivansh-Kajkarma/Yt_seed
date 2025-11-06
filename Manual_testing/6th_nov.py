e00ce21e0487de1cd664340420c9f5bef9f11c65
Shiv-Expert2503 <ssgyanibaba@gmail.com>
2025-11-06 18:39:02 +0530

    outputs

--- Manual_testing/6th_nov.py
+++ Manual_testing/6th_nov.py
@@ -1,6 +1,7 @@
 from pymongo import MongoClient
 import certifi
 
-client = MongoClient("mongodb+srv://uishivansh2503_db_user:3svnnxBJ04eu4iFb@kajkarma.wapdfls.mongodb.net/?appName=Kajkarma",
-                     tls=True, tlsCAFile=certifi.where())
-print(client.server_info())  # should print build info if OK
+
+uri = "mongodb+srv://uishivansh2503_db_user:3svnnxBJ04eu4iFb@kajkarma.wapdfls.mongodb.net/?appName=Kajkarma"
+client = MongoClient(uri, tls=True, tlsCAFile=certifi.where(), serverSelectionTimeoutMS=30000)
+print(client.server_info())  # should print a dict
