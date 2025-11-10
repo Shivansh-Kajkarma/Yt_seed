5d36469f34b4bb27ba8cba5856d3a094c23909c5
Shiv-Expert2503 <ssgyanibaba@gmail.com>
2025-11-10 20:02:47 +0530

    feat: only for production

--- Manual_testing/6th_nov.py
+++
@@ -1,7 +0,0 @@
-from pymongo import MongoClient
-import certifi
-
-
-uri = "mongodb+srv://uishivansh2503_db_user:3svnnxBJ04eu4iFb@kajkarma.wapdfls.mongodb.net/?appName=Kajkarma"
-client = MongoClient(uri, tls=True, tlsCAFile=certifi.where(), serverSelectionTimeoutMS=30000)
-print(client.server_info())  # should print a dict
